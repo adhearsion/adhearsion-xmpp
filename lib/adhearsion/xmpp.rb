d36661794edb0ad14738b41e26bdd2caf4b4d68a
Ben Klang <bklang@mojolingo.com>
2012-01-21 10:26:44 -0500

    Restructure plugin for simpler layout

--- lib/adhearsion/xmpp.rb
+++ lib/adhearsion/xmpp.rb
@@ -14,13 +14,27 @@ require "adhearsion/xmpp/version"
 require "adhearsion/xmpp/plugin"
 
 module Adhearsion
-  module XMPP
+  ##
+  # Adhearsion Plugin that defines the XMPP configuration options
+  # and includes a hook to start the XMPP service in Adhearsion initialization process
+  class XMPP < Adhearsion::Plugin
     extend ActiveSupport::Autoload
-    autoload :Plugin
     autoload :Initializer
+    autoload :Connection
 
-    class << self
-      delegate :client, :to => Initializer
+    # Default configuration for XMPP connection.
+    config :xmpp do
+      use_punchblock true       , :desc => "XMPP connection: re-use Punchblock's XMPP connection. Boolean."
+      jid            nil        , :desc => "XMPP connection: client/component JID to connect to. Must be a string"
+      password       nil        , :desc => "XMPP connection: password identifier. Must be a string"
+      server         "localhost", :desc => "XMPP connection: destination host. Must be a string"
+      port           5222       , :desc => "XMPP connection: destination port. Must be an integer"
+
+    end
+
+    # Include the XMPP service in plugins initialization process
+    init :xmpp do
+      Initializer.new
     end
   end
 end
--- lib/adhearsion/xmpp/initializer.rb
+++ lib/adhearsion/xmpp/initializer.rb
@@ -1,53 +1,46 @@
  module Adhearsion
   module XMPP
     class Initializer
-      class << self
-        def start
-          config.use_punchblock ? start_punchblock : start_blather
-        end
+      def initialize
+        @config ||= Adhearsion.config[:xmpp]
+      end
 
-        def start_punchblock
-          # Nothing needed here; everything is delegated
-          nil
-        end
+      def run
+        @config.use_punchblock ? run_punchblock : run_blather
+      end
 
-        def start_blather
-          raise "Must supply a jid argument to the XMPP configuration" if (config.jid.nil? || config.jid.empty?)
-          raise "Must supply a password argument to the XMPP configuration" if (config.password.nil? || config.password.empty?)
-          raise "Must supply a server argument to the XMPP configuration" if (config.server.nil? || config.server.empty?)
-          raise "Must supply a valid port to the XMPP configuration" unless config.port.is_a? Integer
+      def start_punchblock
+        # Nothing needed here; everything is delegated
+        nil
+      end
 
-          Connection.extend Blather::DSL
-raise "WTF?"
-          Connection.start config.jid, config.password, config.server, config.port
-        end
+      def start_blather
+        raise "Must supply a jid argument to the XMPP configuration" if (@config.jid.nil? || @config.jid.empty?)
+        raise "Must supply a password argument to the XMPP configuration" if (@config.password.nil? || @config.password.empty?)
+        raise "Must supply a server argument to the XMPP configuration" if (@config.server.nil? || @config.server.empty?)
+        raise "Must supply a valid port to the XMPP configuration" unless @config.port.is_a? Integer
 
-        ##
-        # Stop the XMPP connection
-        def stop
-          config.use_punchblock ? stop_punchblock : stop_blather
-        end
+        Connection.extend Blather::DSL
+        Connection.start @config.jid, @config.password, @config.server, @config.port
+      end
 
-        def stop_punchblock
-          # We don't actually want to stop Punchblock from here.
-          nil
-        end
+      ##
+      # Stop the XMPP connection
+      def stop
+        @config.use_punchblock ? stop_punchblock : stop_blather
+      end
 
-        def stop_blather
-          Connection.stop
-        end
+      def stop_punchblock
+        # We don't actually want to stop Punchblock from here.
+        nil
+      end
 
-        def client
-          config.use_punchblock ? PunchblockPlugin.client : Connection.client
-        end
+      def stop_blather
+        Connection.stop
+      end
 
-        private
-
-        ##
-        # Access to XMPP plugin configuration
-        def config
-          @config ||= Adhearsion.config[:xmpp]
-        end
+      def client
+        @config.use_punchblock ? PunchblockPlugin.client : Connection.client
       end
     end
   end
