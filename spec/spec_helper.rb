cbb948c43876ebb7250f401bd8e7c26b04276c1f
Ben Klang <bklang@mojolingo.com>
2012-01-20 19:12:02 -0500

    Provide placeholder for PunchblockPlugin

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 require 'adhearsion/xmpp'
 
+module Adhearsion
+  class PunchblockPlugin
+  end
+end
+
 def reset_adhearsion_xmpp_config
   Adhearsion.config[:adhearsion_xmpp].jid      = nil
   Adhearsion.config[:adhearsion_xmpp].password = nil
