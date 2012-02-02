31d2437748e4831255a464c237d2cbd7d7190873
Ben Klang <bklang@mojolingo.com>
2012-02-01 19:26:05 -0500

    Let dependencies be described by gemspec

--- lib/adhearsion/xmpp.rb
+++ lib/adhearsion/xmpp.rb
@@ -1,15 +1,5 @@
 require "adhearsion"
 require "active_support/dependencies/autoload"
-
-begin
-  require 'blather/client/client'
-  require 'blather/client/dsl'
-rescue LoadError
-  logger.fatal "XMPP support requires the \"blather\" gem."
-  # Silence the abort so we don't get an ugly backtrace
-  abort ""
-end
-
 require "adhearsion/xmpp/version"
 require "adhearsion/xmpp/plugin"
 
